526db5d85e37731ed6564372301726effee4fa91
MoiMarquez <96758329+MoiMarquez@users.noreply.github.com>
2022-08-03 11:25:47 -0500

    Initial commit

---
+++ src/test/java/com/Alkemy/Disney/disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alkemy.Disney.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
